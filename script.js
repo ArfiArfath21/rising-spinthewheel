2abb8159eee43573cd38ea71f696bb166e984242
arfsyed <arfath.syed@unilever.com>
2024-03-22 02:15:39 +0530

    v1.1

--- script.js
+++ script.js
@@ -64,6 +64,16 @@ function getCurrentRotationAngle() {
   }
 
 const  sections_count = 8
+const sections = {
+  0: 'python',
+  1: 'diversity',
+  2: 'data',
+  3: 'ai',
+  4: 'inclusion',
+  5: 'test',
+  6: 'check',
+  7: 'work'
+};
 // Function to start or stop the rotation
 const toggleRotation = () => {
 rotationSpeed = getRandomInt(30,20)
@@ -77,7 +87,7 @@ rotationSpeed = getRandomInt(30,20)
       }
     console.log(rotationAngle);
     var division = Math.floor(rotationAngle/(360/sections_count));
-    generateQRCode(division)
+    generateQRCode(sections[division])
   } else {
     document.getElementById("qrcode").innerHTML=""
     document.getElementById('categorySelected').innerHTML = "";
@@ -89,12 +99,12 @@ rotationSpeed = getRandomInt(30,20)
 };
 toggleButton.addEventListener("click", toggleRotation);
 
-function generateQRCode(number) {
-    var url = `https://www.google.com/search?q=${number}`
+function generateQRCode(section) {
+    const now = new Date();
+    url = `https://risingps2024.streamlit.app/quiz?userId=${now.getDate()}${now.getHours()}${now.getMinutes()}${now.getSeconds()}?quizSection=${section}`
     console.log(url)
-    url = "https://risingps2024.streamlit.app/pythonpage"
+    // url = `https://risingps2024.streamlit.app/quiz?userId=${now.getDate()}${now.getHours()}${now.getMinutes()}${now.getSeconds()}?quizSection=python`
     const qrcode = new QRCode("qrcode",url);
-    console.log(url)
-    document.getElementById('categorySelected').innerHTML = `Your Category is ${number}`;
+    document.getElementById('categorySelected').innerHTML = `Your Category is ${section}`;
   }
 
